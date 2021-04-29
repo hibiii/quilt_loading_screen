1fd618db880670de41c8e270cafe1d8933bdf3a0
darkerbit <mail.darkerbit@gmail.com>
2021-04-29 15:59:22 +0300

    Rotation!

--- src/main/java/io/github/darkerbit/quiltloadingscreen/QuiltLoadingScreen.java
+++ src/main/java/io/github/darkerbit/quiltloadingscreen/QuiltLoadingScreen.java
@@ -5,6 +5,9 @@ import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.hud.BackgroundHelper;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.Matrix3f;
+import net.minecraft.util.math.Matrix4f;
+import net.minecraft.util.math.Quaternion;
 
 public class QuiltLoadingScreen {
     public static final int BACKGROUND_COLOR = BackgroundHelper.ColorMixer.getArgb(0, 35, 22, 56);
@@ -13,10 +16,11 @@ public class QuiltLoadingScreen {
             new Identifier("quilt-loading-screen", "textures/gui/patch.png");
 
     private static final int PATCH_SIZE = 32;
-    private static final int PATCH_SIZE_TEX = 128;
     private static final int PATCH_COUNT = 8;
 
     private final MinecraftClient client;
+
+    private double rot = 0;
 
     public QuiltLoadingScreen(MinecraftClient client) {
         this.client = client;
@@ -25,27 +29,34 @@ public class QuiltLoadingScreen {
     public void renderPatches(MatrixStack matrices, float delta) {
         client.getTextureManager().bindTexture(PATCH_TEXTURE);
 
-        renderPatch(matrices, 64, 64, 45, 3);
+        renderPatch(matrices, 64, 64, rot, 3);
+
+        rot += delta * 2;
     }
 
     public static void renderPatch(MatrixStack matrices, double x, double y, double rot, int type) {
         matrices.push();
         matrices.translate(x, y, 0);
 
-        double r = Math.toRadians(rot);
+        Matrix4f matrix = matrices.peek().getModel();
+
+        float r = (float) Math.toRadians(rot);
+        Quaternion quaternion = new Quaternion(0.0f, 0.0f, r, false);
+
+        matrix.multiply(quaternion);
 
         double x1 = -PATCH_SIZE / (double) 2;
         double y1 = -PATCH_SIZE / (double) 2;
         double x2 = PATCH_SIZE / (double) 2;
         double y2 = PATCH_SIZE / (double) 2;
 
-        // Yarn mislabeled these arguments, it's x0 x1 y0 y1 not x0 y0 x1 y 1
-        DrawableHelperAccessor.quiltLoadingScreen$drawTexture(
-                matrices,
+        float u0 = 1.0f / PATCH_COUNT * type;
+        float u1 = u0 + 1.0f / PATCH_COUNT;
+
+        DrawableHelperAccessor.quiltLoadingScreen$drawTexturedQuad(
+                matrix,
                 (int) x1, (int) x2, (int) y1, (int) y2, 0,
-                PATCH_SIZE_TEX, PATCH_SIZE_TEX,
-                type * PATCH_SIZE_TEX, 0,
-                PATCH_COUNT * PATCH_SIZE_TEX, PATCH_SIZE_TEX
+                u0, u1, 0.0f, 1.0f
         );
 
         matrices.pop();
--- src/main/java/io/github/darkerbit/quiltloadingscreen/mixin/DrawableHelperAccessor.java
+++ src/main/java/io/github/darkerbit/quiltloadingscreen/mixin/DrawableHelperAccessor.java
@@ -2,13 +2,14 @@ package io.github.darkerbit.quiltloadingscreen.mixin;
 
 import net.minecraft.client.gui.DrawableHelper;
 import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.util.math.Matrix4f;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Invoker;
 
 @Mixin(DrawableHelper.class)
 public interface DrawableHelperAccessor {
-    @Invoker("drawTexture")
-    static void quiltLoadingScreen$drawTexture(MatrixStack matrices, int x0, int y0, int x1, int y1, int z, int regionWidth, int regionHeight, float u, float v, int textureWidth, int textureHeight) {
-        throw new RuntimeException("If you're seeing this, Mixin is horribly broken.");
+    @Invoker("drawTexturedQuad")
+    static void quiltLoadingScreen$drawTexturedQuad(Matrix4f matrices, int x0, int x1, int y0, int y1, int z, float u0, float u1, float v0, float v1) {
+        throw new UnsupportedOperationException();
     }
 }
